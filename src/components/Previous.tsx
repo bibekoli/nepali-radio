d14d0839b4fb326660f8ac1d7a502a7a257a5235
bibekoli <git@bibekoli.com>
2024-12-08 19:15:36 +0545

    Add SVG icon components for Heart, HeartSolid, Next, Pause, Play, Previous, Sound, Spinner, and Stop

---
+++ src/components/Previous.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import type { SVGProps } from "react";
+
+export default function Previous(props: SVGProps<SVGSVGElement>) {
+	return (<svg xmlns="http://www.w3.org/2000/svg" width={14} height={14} viewBox="0 0 14 14" {...props}><path fill="currentColor" fillRule="evenodd" d="m4.238 6.056l7.819-5.4c.396-.25.878-.183 1.226.08c.269.201.458.519.467.897v10.705c0 .884-.979 1.456-1.693 1.004l-7.82-5.367c-.65-.439-.65-1.48 0-1.919ZM0 1.742a1 1 0 1 1 2 0v10.516a1 1 0 1 1-2 0z" clipRule="evenodd"></path></svg>);
+}
---
+++ src/components/Stop.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import type { SVGProps } from "react";
+
+export default function Stop(props: SVGProps<SVGSVGElement>) {
+	return (<svg xmlns="http://www.w3.org/2000/svg" width={14} height={14} viewBox="0 0 14 14" {...props}><path fill="currentColor" fillRule="evenodd" d="M1.5 0A1.5 1.5 0 0 0 0 1.5v11A1.5 1.5 0 0 0 1.5 14h11a1.5 1.5 0 0 0 1.5-1.5v-11A1.5 1.5 0 0 0 12.5 0z" clipRule="evenodd"></path></svg>);
+}
