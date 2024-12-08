b677339936c7bfc7b27c9d256ffc23e8fd6f73d9
bibekoli <git@bibekoli.com>
2024-12-08 19:17:48 +0545

    Add custom font and update document title in _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,22 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Delius as Font } from "next/font/google";
+import Head from "next/head";
+
+const font = Font({
+  subsets: ["latin"],
+  weight: ["400"]
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>BiTunes - Listen to your favorite Nepali radio stations</title>
+      </Head>
+      <main className={font.className}>
+        <Component {...pageProps} />
+      </main>
+    </>
+  );
 }
